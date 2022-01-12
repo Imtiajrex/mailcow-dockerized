3b9e1b06047364f0c7102669b263a8c60360b3ec
FreddlePat <p.schult95@outlook.de>
2022-01-12 21:09:18 +0100

    migrating from u2f-api.js to webauthn

--- data/web/inc/lib/WebAuthn/Attestation/Format/AndroidKey.php
+++ data/web/inc/lib/WebAuthn/Attestation/Format/AndroidKey.php
@@ -1,13 +1,12 @@
 <?php
 
-
 namespace lbuchs\WebAuthn\Attestation\Format;
 use lbuchs\WebAuthn\Attestation\AuthenticatorData;
 use lbuchs\WebAuthn\WebAuthnException;
 use lbuchs\WebAuthn\Binary\ByteBuffer;
 
-class U2f extends FormatBase {
-    private $_alg = -7;
+class AndroidKey extends FormatBase {
+    private $_alg;
     private $_signature;
     private $_x5c;
 
@@ -17,15 +16,15 @@ class U2f extends FormatBase {
         // check u2f data
         $attStmt = $this->_attestationObject['attStmt'];
 
-        if (\array_key_exists('alg', $attStmt) && $attStmt['alg'] !== $this->_alg) {
-            throw new WebAuthnException('u2f only accepts algorithm -7 ("ES256"), but got ' . $attStmt['alg'], WebAuthnException::INVALID_DATA);
+        if (!\array_key_exists('alg', $attStmt) || $this->_getCoseAlgorithm($attStmt['alg']) === null) {
+            throw new WebAuthnException('unsupported alg: ' . $attStmt['alg'], WebAuthnException::INVALID_DATA);
         }
 
         if (!\array_key_exists('sig', $attStmt) || !\is_object($attStmt['sig']) || !($attStmt['sig'] instanceof ByteBuffer)) {
             throw new WebAuthnException('no signature found', WebAuthnException::INVALID_DATA);
         }
 
-        if (!\array_key_exists('x5c', $attStmt) || !\is_array($attStmt['x5c']) || \count($attStmt['x5c']) !== 1) {
+        if (!\array_key_exists('x5c', $attStmt) || !\is_array($attStmt['x5c']) || \count($attStmt['x5c']) < 1) {
             throw new WebAuthnException('invalid x5c certificate', WebAuthnException::INVALID_DATA);
         }
 
@@ -33,8 +32,16 @@ class U2f extends FormatBase {
             throw new WebAuthnException('invalid x5c certificate', WebAuthnException::INVALID_DATA);
         }
 
+        $this->_alg = $attStmt['alg'];
         $this->_signature = $attStmt['sig']->getBinaryString();
         $this->_x5c = $attStmt['x5c'][0]->getBinaryString();
+
+        if (count($attStmt['x5c']) > 1) {
+            for ($i=1; $i<count($attStmt['x5c']); $i++) {
+                $this->_x5c_chain[] = $attStmt['x5c'][$i]->getBinaryString();
+            }
+            unset ($i);
+        }
     }
 
 
@@ -43,10 +50,7 @@ class U2f extends FormatBase {
      * @return string
      */
     public function getCertificatePem() {
-        $pem = '-----BEGIN CERTIFICATE-----' . "\n";
-        $pem .= \chunk_split(\base64_encode($this->_x5c), 64, "\n");
-        $pem .= '-----END CERTIFICATE-----' . "\n";
-        return $pem;
+        return $this->_createCertificatePem($this->_x5c);
     }
 
     /**
@@ -59,12 +63,10 @@ class U2f extends FormatBase {
             throw new WebAuthnException('invalid public key: ' . \openssl_error_string(), WebAuthnException::INVALID_PUBLIC_KEY);
         }
 
-        // Let verificationData be the concatenation of (0x00 || rpIdHash || clientDataHash || credentialId || publicKeyU2F)
-        $dataToVerify = "\x00";
-        $dataToVerify .= $this->_authenticatorData->getRpIdHash();
+        // Verify that sig is a valid signature over the concatenation of authenticatorData and clientDataHash
+        // using the attestation public key in attestnCert with the algorithm specified in alg.
+        $dataToVerify = $this->_authenticatorData->getBinary();
         $dataToVerify .= $clientDataHash;
-        $dataToVerify .= $this->_authenticatorData->getCredentialId();
-        $dataToVerify .= $this->_authenticatorData->getPublicKeyU2F();
 
         $coseAlgorithm = $this->_getCoseAlgorithm($this->_alg);
 
@@ -91,3 +93,4 @@ class U2f extends FormatBase {
         return $v;
     }
 }
+
